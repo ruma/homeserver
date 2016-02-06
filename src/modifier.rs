03ed42c85fe77e42b12d42fe7bad7a500641536d
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-02-06 06:21:33 -0800

    Set the Content-Type header in responses.

--- src/modifier.rs
+++ src/modifier.rs
@@ -1,4 +1,5 @@
 use iron::Response;
+use iron::headers::ContentType;
 use iron::modifier::Modifier;
 use serde::Serialize;
 use serde_json::to_string;
@@ -8,6 +9,7 @@ pub struct SerializableResponse<T: Serialize>(pub T);
 
 impl<T> Modifier<Response> for SerializableResponse<T> where T: Serialize {
     fn modify(self, response: &mut Response) {
+        response.headers.set(ContentType::json());
         response.body = Some(Box::new(to_string(&self.0).expect("could not serialize response data")));
     }
 }
