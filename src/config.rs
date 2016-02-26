0385f8edea213fdfeaf5217e381216fd614fc5b8
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-02-26 08:29:06 -0800

    Add macaroon_secret_key to Config.

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::Read;
 use std::sync::Arc;
 
+use base64::u8de;
 use iron::{Plugin, Request};
 use iron::typemap::Key;
 use persistent::Read as PersistentRead;
@@ -22,10 +23,21 @@ pub fn load(path: &str) -> Result<FinalConfig, CLIError> {
         Err(error) => return Err(From::from(error)),
     };
 
+    let macaroon_secret_key = match u8de(config.macaroon_secret_key.as_bytes()) {
+        Ok(bytes) => match bytes.len() {
+            32 => bytes,
+            _ => return Err(CLIError::new( "macaroon_secret_key must be 32 bytes.")),
+        },
+        Err(error) => return Err(CLIError::new(
+            "macaroon_secret_key must be valid Base64."
+        )),
+    };
+
     Ok(FinalConfig {
         bind_address: config.bind_address.unwrap_or("127.0.0.1".to_owned()),
         bind_port: config.bind_port.unwrap_or("3000".to_owned()),
         domain: config.domain,
+        macaroon_secret_key: macaroon_secret_key,
         postgres_url: config.postgres_url,
     })
 }
@@ -44,7 +56,13 @@ pub struct Config {
     pub bind_port: Option<String>,
     /// The DNS name where clients can reach the server. Used as the hostname portion of user IDs.
     pub domain: String,
-    /// A [PostgreSQL connection string](http://www.postgresql.org/docs/current/static/libpq-connect.html#LIBPQ-CONNSTRING) for Ruma's PostgreSQL database.
+    /// The secret key used for generating
+    /// [Macaroons](https://research.google.com/pubs/pub41892.html). Must be 32
+    /// cryptographically random bytes, encoded as a Base64 string. Changing this value will
+    /// invalidate any previously generated macaroons.
+    pub macaroon_secret_key: String,
+    /// A [PostgreSQL connection string](http://www.postgresql.org/docs/current/static/libpq-connect.html#LIBPQ-CONNSTRING)
+    /// for Ruma's PostgreSQL database.
     pub postgres_url: String,
 }
 
@@ -56,6 +74,7 @@ pub struct FinalConfig {
     pub bind_address: String,
     pub bind_port: String,
     pub domain: String,
+    pub macaroon_secret_key: Vec<u8>,
     pub postgres_url: String,
 }
 
