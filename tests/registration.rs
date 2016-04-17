d4fe79ea2e5f8b0c306b950bf2e154d0d7d41042
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-04-17 03:41:56 -0700

    Add test for registration with all input parameters.

--- tests/registration.rs
+++ tests/registration.rs
@@ -13,3 +13,17 @@ fn minimum_input_parameters() {
     assert_eq!(response.json.find("home_server").unwrap().as_string().unwrap(), "ruma.test");
     assert!(response.json.find("user_id").is_some());
 }
+
+#[test]
+fn all_input_parameters() {
+    let test = Test::new();
+
+    let response = test.post(
+        "/_matrix/client/r0/register?kind=user",
+        r#"{"bind_email": true, "username":"carl", "password": "secret"}"#,
+    );
+
+    assert!(response.json.find("access_token").is_some());
+    assert_eq!(response.json.find("home_server").unwrap().as_string().unwrap(), "ruma.test");
+    assert_eq!(response.json.find("user_id").unwrap().as_string().unwrap(), "carl");
+}
