f70848822abc82c0c85fc5265fe3d313ecb6473e
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-09-14 00:33:53 -0700

    Add tests for message event creation and fix bugs in the implementation.

--- src/api/r0/event_creation.rs
+++ src/api/r0/event_creation.rs
@@ -6,18 +6,19 @@ use bodyparser;
 use diesel::{Connection, ExecuteDsl, FindDsl, LoadDsl, insert};
 use iron::{Chain, Handler, IronError, IronResult, Plugin, Request, Response, status};
 use router::Router;
-use ruma_identifiers::{EventId, RoomId, UserId};
+use ruma_identifiers::{EventId, RoomId};
 use ruma_events::{CustomRoomEvent, EventType};
 use ruma_events::call::answer::AnswerEvent;
 use ruma_events::call::candidates::CandidatesEvent;
 use ruma_events::call::hangup::HangupEvent;
 use ruma_events::call::invite::InviteEvent;
 use ruma_events::room::message::MessageEvent;
-use serde_json::from_value;
+use serde::Deserialize;
+use serde_json::{Value, from_value};
 
 use db::DB;
 use config::Config;
-use error::ApiError;
+use error::{ApiError, MapApiError};
 use event::NewEvent;
 use middleware::{AccessTokenAuth, JsonRequest};
 use modifier::SerializableResponse;
@@ -50,7 +51,9 @@ impl Handler for SendMessageEvent {
         let params = request.extensions.get::<Router>().expect("Params object is missing").clone();
 
         let room_id = match params.find("room_id") {
-            Some(room_id) => RoomId::try_from(room_id).map_err(ApiError::from)?,
+            Some(room_id) => RoomId::try_from(room_id).map_api_err(|_| {
+                ApiError::not_found(Some(&format!("No room found with ID {}", room_id)))
+            })?,
             None => {
                 let error = ApiError::missing_param("room_id");
 
@@ -75,64 +78,64 @@ impl Handler for SendMessageEvent {
             .expect("JsonRequest verifies the Result is Ok")
             .expect("JsonRequest verifies the Option is Some");
         let config = Config::from_request(request)?;
-        let event_id = EventId::new(&config.domain).map_err(ApiError::from)?;
-        let user_id = UserId::try_from(&format!("@{}:{}", user.id, config.domain))
-            .map_err(ApiError::from)?;
+        let event_id = EventId::new(&config.domain).map_api_err(|_| {
+            ApiError::unknown(Some("Failed to generated event ID for the new event."))
+        })?;
 
         let room_event: NewEvent = match event_type {
             EventType::CallAnswer => {
                 AnswerEvent {
-                    content: from_value(event_content).map_err(ApiError::from)?,
+                    content: extract_event_content(event_content, &event_type)?,
                     event_id: event_id.clone(),
                     extra_content: (),
                     event_type: event_type.clone(),
                     room_id: room_id.clone(),
                     unsigned: None,
-                    user_id: user_id.clone(),
+                    user_id: user.id.clone(),
                 }.try_into().map_err(ApiError::from)?
             }
             EventType::CallCandidates => {
                 CandidatesEvent {
-                    content: from_value(event_content).map_err(ApiError::from)?,
+                    content: extract_event_content(event_content, &event_type)?,
                     event_id: event_id.clone(),
                     extra_content: (),
                     event_type: event_type.clone(),
                     room_id: room_id.clone(),
                     unsigned: None,
-                    user_id: user_id.clone(),
+                    user_id: user.id.clone(),
                 }.try_into().map_err(ApiError::from)?
             }
             EventType::CallHangup => {
                 HangupEvent {
-                    content: from_value(event_content).map_err(ApiError::from)?,
+                    content: extract_event_content(event_content, &event_type)?,
                     event_id: event_id.clone(),
                     extra_content: (),
                     event_type: event_type.clone(),
                     room_id: room_id.clone(),
                     unsigned: None,
-                    user_id: user_id.clone(),
+                    user_id: user.id.clone(),
                 }.try_into().map_err(ApiError::from)?
             }
             EventType::CallInvite => {
                 InviteEvent {
-                    content: from_value(event_content).map_err(ApiError::from)?,
+                    content: extract_event_content(event_content, &event_type)?,
                     event_id: event_id.clone(),
                     extra_content: (),
                     event_type: event_type.clone(),
                     room_id: room_id.clone(),
                     unsigned: None,
-                    user_id: user_id.clone(),
+                    user_id: user.id.clone(),
                 }.try_into().map_err(ApiError::from)?
             }
             EventType::RoomMessage => {
                 MessageEvent {
-                    content: from_value(event_content).map_err(ApiError::from)?,
+                    content: extract_event_content(event_content, &event_type)?,
                     event_id: event_id.clone(),
                     extra_content: (),
                     event_type: event_type.clone(),
                     room_id: room_id.clone(),
                     unsigned: None,
-                    user_id: user_id.clone(),
+                    user_id: user.id.clone(),
                 }.try_into().map_err(ApiError::from)?
             }
             EventType::Custom(ref custom_event_type) => {
@@ -143,11 +146,13 @@ impl Handler for SendMessageEvent {
                     event_type: EventType::Custom(custom_event_type.clone()),
                     room_id: room_id.clone(),
                     unsigned: None,
-                    user_id: user_id.clone(),
+                    user_id: user.id.clone(),
                 }.try_into().map_err(ApiError::from)?
             }
             _ => {
-                let error = ApiError::bad_event(None);
+                let error = ApiError::bad_event(
+                    Some(&format!("Events of type {} cannot be created with this API.", event_type))
+                );
 
                 return Err(IronError::new(error.clone(), error));
             }
@@ -160,7 +165,7 @@ impl Handler for SendMessageEvent {
             let power_levels = room.current_power_levels(&*connection)?;
             let user_power_level = power_levels
                 .users
-                .get(&user_id)
+                .get(&user.id)
                 .unwrap_or(&power_levels.users_default);
             let required_power_level = power_levels
                 .events
@@ -182,5 +187,136 @@ impl Handler for SendMessageEvent {
         };
 
         Ok(Response::with((status::Ok, SerializableResponse(response))))
+    }
+}
+
+fn extract_event_content<T: Deserialize>(event_content: Value, event_type: &EventType)
+-> Result<T, ApiError> {
+    from_value(event_content).map_api_err(|_| {
+        ApiError::bad_event(
+            Some(
+                &format!(
+                    "Event content did not match expected structure for event of type {}.",
+                    event_type
+                )
+            )
+        )
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use test::Test;
+
+    #[test]
+    fn create_message_event() {
+        let test = Test::new();
+        let access_token = test.create_access_token();
+
+        let create_room_path = format!(
+            "/_matrix/client/r0/createRoom?access_token={}",
+            access_token
+        );
+
+        let create_room_response = test.post(&create_room_path, "{}");
+
+        let room_id = create_room_response.json().find("room_id").unwrap().as_str().unwrap();
+
+        let create_event_path = format!(
+            "/_matrix/client/r0/rooms/{}/send/m.room.message/1?access_token={}",
+            room_id,
+            access_token
+        );
+
+        let response = test.put(&create_event_path, r#"{"body":"Hi","msgtype":"m.text"}"#);
+
+        assert!(response.json().find("event_id").unwrap().as_str().is_some());
+    }
+
+    #[test]
+    fn event_content_does_not_match_event_type() {
+        let test = Test::new();
+        let access_token = test.create_access_token();
+
+        let create_room_path = format!(
+            "/_matrix/client/r0/createRoom?access_token={}",
+            access_token
+        );
+
+        let create_room_response = test.post(&create_room_path, "{}");
+
+        let room_id = create_room_response.json().find("room_id").unwrap().as_str().unwrap();
+
+        let create_event_path = format!(
+            "/_matrix/client/r0/rooms/{}/send/m.call.answer/1?access_token={}",
+            room_id,
+            access_token
+        );
+
+        let response = test.put(&create_event_path, r#"{"body":"Hi","msgtype":"m.text"}"#);
+
+        let json = response.json();
+
+        assert_eq!(json.find("errcode").unwrap().as_str().unwrap(), "IO.RUMA_BAD_EVENT");
+        assert_eq!(
+            json.find("error").unwrap().as_str().unwrap(),
+            "Event content did not match expected structure for event of type m.call.answer."
+        );
+    }
+
+    #[test]
+    fn non_message_event_type() {
+        let test = Test::new();
+        let access_token = test.create_access_token();
+
+        let create_room_path = format!(
+            "/_matrix/client/r0/createRoom?access_token={}",
+            access_token
+        );
+
+        let create_room_response = test.post(&create_room_path, "{}");
+
+        let room_id = create_room_response.json().find("room_id").unwrap().as_str().unwrap();
+
+        let create_event_path = format!(
+            "/_matrix/client/r0/rooms/{}/send/m.room.topic/1?access_token={}",
+            room_id,
+            access_token
+        );
+
+        let response = test.put(&create_event_path, r#"{"topic":"fail"}"#);
+
+        let json = response.json();
+
+        assert_eq!(json.find("errcode").unwrap().as_str().unwrap(), "IO.RUMA_BAD_EVENT");
+        assert_eq!(
+            json.find("error").unwrap().as_str().unwrap(),
+            "Events of type m.room.topic cannot be created with this API."
+        );
+    }
+
+    #[test]
+    fn custom_message_event() {
+        let test = Test::new();
+        let access_token = test.create_access_token();
+
+        let create_room_path = format!(
+            "/_matrix/client/r0/createRoom?access_token={}",
+            access_token
+        );
+
+        let create_room_response = test.post(&create_room_path, "{}");
+
+        let room_id = create_room_response.json().find("room_id").unwrap().as_str().unwrap();
+
+        let create_event_path = format!(
+            "/_matrix/client/r0/rooms/{}/send/io.ruma.test/1?access_token={}",
+            room_id,
+            access_token
+        );
+
+        let response = test.put(&create_event_path, r#"{"foo":"bar"}"#);
+
+        assert!(response.json().find("event_id").unwrap().as_str().is_some());
     }
 }
