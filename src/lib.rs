d60720a309572f7c11f9aa673ac7fb2f2a294538
Jimmy Cuadra <jimmy@jimmycuadra.com>
2019-06-01 06:47:01 -0700

    Enable additional clippy lints.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,25 @@
     missing_docs,
     warnings
 )]
+#![warn(
+    clippy::empty_line_after_outer_attr,
+    clippy::expl_impl_clone_on_copy,
+    clippy::if_not_else,
+    clippy::items_after_statements,
+    clippy::match_same_arms,
+    clippy::mem_forget,
+    clippy::missing_docs_in_private_items,
+    clippy::multiple_inherent_impl,
+    clippy::mut_mut,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::single_match_else,
+    clippy::unicode_not_nfc,
+    clippy::use_self,
+    clippy::used_underscore_binding,
+    clippy::wrong_pub_self_convention,
+    clippy::wrong_self_convention
+)]
 
 #[macro_use]
 extern crate diesel;
